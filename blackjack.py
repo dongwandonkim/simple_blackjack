945674176be0610a1f783ac024c820a0dd463fc6
Dongwan Kim <dongwan.don.kim@gmail.com>
2023-02-28 00:20:57 -0800

    card and deck class

---
+++ blackjack.py
@@ -0,0 +1,32 @@
+import random
+
+suites = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+    
+    def __str__(self):
+        return self.rank + " of " + self.suit
+    
+class Deck:
+    def __init__(self):
+        self.all_cards = []
+
+        for suit in suites:
+            for rank in ranks:
+                self.all_cards.append(Card(suit, rank))
+
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+
+    def __str__(self):
+        return f"Deck of {len(self.all_cards)} cards"
+        
+
+new_deck = Deck()
+print(new_deck)
